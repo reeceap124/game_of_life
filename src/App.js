51940f8228cb1164c2a32d94202f1a17b8ab7ea8
Reece Pierson <piersonreece@gmail.com>
2020-06-25 18:55:00 -0500

    deletes unused declarations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef, useCallback} from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import useInterval from './customHooks/useInterval'
 import Slider from './components/Slider'
